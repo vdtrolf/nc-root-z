06c4ec05f240892602d219e7d3d4bac0c8944a4c
Rolf van der Tang <rolf@notrecoin.nl>
2026-02-07 07:29:38 +0100

    explicit path

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # This is a bash script to rebuild and deplopy notrecoin content
-/snap/bin/zola build
-cd public
+cd /home/rolf/repos/zola/nc-root-z
+zola build
+cd ./public
 cp -r * /var/www/html
 systemctl stop nginx
 systemctl start nginx
